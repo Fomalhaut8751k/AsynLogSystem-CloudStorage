4068e72c923f2444c5a106e40a71c96459aab3c1
Fomalhaut8751k <1308041515@qq.com>
2025-09-18 20:24:16 +0800

    fourth commit

--- log_system/include/AsyncWorker.hpp
+++ log_system/include/AsyncWorker.hpp
@@ -31,15 +31,21 @@ namespace mylog
         std::condition_variable cond_productor_;  // 用于生产者的同步通信
         std::condition_variable cond_consumer_;   // 用于消费者的同步通信
 
+        std::condition_variable cond_exit_;         // 用于等待生产者消费者都退出后再继续析构
+
         std::shared_ptr<AsyncBuffer> buffer_productor_;
         std::shared_ptr<AsyncBuffer> buffer_consumer_;
 
         std::atomic_bool label_consumer_ready_;  // 给生产者判断消费者是否处在空闲状态
         std::atomic_bool label_data_ready_;      // 给生产者判断数据是否传入
+        std::atomic_bool label_producor_swap_;
 
         LOG_FUNC log_func_;
 
         bool ExitLabel_;
+
+        bool ExitProductorLabel_;
+        bool ExitConsumerLabel_;
 
 
     public:
@@ -48,20 +54,28 @@ namespace mylog
             buffer_consumer_(std::make_shared<AsyncBuffer>()),
             log_func_(log_func),
 
-            label_consumer_ready_(false),
-            label_data_ready_(false)
+            label_consumer_ready_(true),
+            label_data_ready_(false),
+            label_producor_swap_(false)
 
         {
             ExitLabel_ = false;
+            ExitProductorLabel_ = false;
+            ExitConsumerLabel_ = false;
         }
 
         ~AsyncWorker()
         {
+            std::unique_lock<std::mutex> lock(Mutex_);
+            std::cerr << "~AsyncWorker()" << std::endl;
             // 终止生产者和消费者线程
             ExitLabel_ = true;
             // 通知生产者和消费者
             cond_productor_.notify_all();
             cond_consumer_.notify_all();
+
+            // 生产者和消费者中最后一个结束的时候的通知会让这里从等待到阻塞
+            cond_exit_.wait(lock, [this]()->bool { return ExitProductorLabel_ && ExitConsumerLabel_;});  
         }
 
         // 启动
@@ -91,7 +105,9 @@ namespace mylog
                 );
                 if(ExitLabel_)
                 {
-                    // std::cerr << "producer thread exit" << std::endl;
+                    std::cerr << "producer thread exit" << std::endl;
+                    ExitProductorLabel_ = true;
+                    cond_exit_.notify_all();
                     return;
                 }
 
@@ -103,8 +119,9 @@ namespace mylog
                     buffer_productor_ = buffer_consumer_;
                     buffer_consumer_ = tmp_buffer_controler;
                     
+                    label_consumer_ready_ = false;
+                    
                     label_data_ready_ = false;
-                
                     // 此时消费者的buffer就有了数据
                     cond_consumer_.notify_all();
                 }
@@ -118,27 +135,32 @@ namespace mylog
         }
 
         void consumerTask()   // 消费者线程
-        {
-            // std::this_thread::sleep_for(std::chrono::seconds(12));
+        { 
             while(!ExitLabel_)
             {
                 std::unique_lock<std::mutex> lock(Mutex_);
                 label_consumer_ready_ = true;
                 cond_productor_.notify_all();  // 通知生产者现在消费者空闲状态
                 // 只要生产者一声令下，消费者就干活
-                cond_consumer_.wait(lock);
+                cond_consumer_.wait(lock, [&]()->bool { return ExitLabel_ || !label_consumer_ready_;});
                 if(ExitLabel_)
                 {
-                    // std::cerr << "consumer thread exit" << std::endl;
+                    std::cerr << "consumer thread exit" << std::endl;
+                    ExitConsumerLabel_ = true;
+                    cond_exit_.notify_all();
                     return;
                 }
 
                 label_consumer_ready_ = false;
+
                 for(std::string message_formatted: buffer_consumer_->read())
                 {
                     // 把日志消息发送到指定的位置
                     log_func_(message_formatted);  
                 }
+
+                // // 假设消费者处理数据有时间
+                // std::this_thread::sleep_for(std::chrono::seconds(1));
 
                 buffer_consumer_->clear();
             }
@@ -152,7 +174,7 @@ namespace mylog
             std::unique_lock<std::mutex> lock(Mutex_);
             {
                 buffer_productor_->write(buffer, buffer_length);
-                std::cerr << "read from user" << std::endl;
+                // std::cerr << "read from user" << std::endl;
                 label_data_ready_ = true;
                 cond_productor_.notify_all();
             } 
